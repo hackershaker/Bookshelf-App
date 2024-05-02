0a9decdc3a5ffb15e4635ce69644de902078a1f9
DY <deltahackall@gmail.com>
2024-05-02 16:52:39 +0900

    apply ime padding to column

--- app/src/main/java/com/example/bookshelf/ui/screen/StartScreen.kt
+++ app/src/main/java/com/example/bookshelf/ui/screen/StartScreen.kt
@@ -1,13 +1,20 @@
 package com.example.bookshelf.ui.screen
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.GenericShape
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -16,26 +23,21 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Rect
-import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.bookshelf.R
 import com.example.compose.BookShelfTheme
 
-private val cropShape =
-    GenericShape { size: Size, layoutDirection: androidx.compose.ui.unit.LayoutDirection ->
-        addRect(Rect(1f, 1f, 1f, 1f))
-    }
-
-private val TAG = "StartScreen"
+private const val TAG = "StartScreen"
 
 // 메인 화면, 검색 창
 @Composable
@@ -45,37 +47,68 @@ fun StartScreen(
     text: MutableState<String>,
     keyboardSearchOnClick: () -> Unit,
     snackbarHostState: SnackbarHostState = SnackbarHostState(),
+    density: Density = LocalDensity.current,
 ) {
-    val snackbarModifier = Modifier
+    val imeInsets = WindowInsets.ime
+    val scrollState = rememberScrollState()
+
+    Log.d(
+        TAG, "키보드 크기\n" +
+                "top: ${imeInsets.getTop(density)}\n" +
+                "bottom ${imeInsets.getBottom(density)}\n" +
+                "left: ${imeInsets.getLeft(density, LayoutDirection.Ltr)}\n" +
+                "right: ${imeInsets.getRight(density, LayoutDirection.Ltr)}\n"
+    )
+
 
     Scaffold(
-        modifier = modifier,
-        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+        modifier = modifier
+            .imePadding(),
+        snackbarHost = {
+            SnackbarHost(
+                hostState = snackbarHostState,
+                modifier = Modifier,
+            )
+        },
     ) { padding ->
+//        ime padding을 column에 적용하려면 scrollable 해야 한다.
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(padding),
+                .padding(padding)
+                .imePadding()
+                .verticalScroll(scrollState),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text(text = stringResource(R.string.main_screen_title))
+            Text(text = stringResource(R.string.main_screen_title), fontSize = 48.sp)
             Image(
+                modifier = Modifier,
                 painter = painterResource(id = R.drawable.book_image),
                 contentDescription = stringResource(R.string.main_screen_image_description),
-                modifier = Modifier,
             )
-            TextField(
-                value = text.value,
-                onValueChange = { text.value = it },
-                label = { Text(text = stringResource(R.string.main_screen_searchbar_label)) },
-                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
-                keyboardActions = KeyboardActions(onSearch = {
-                    keyboardSearchOnClick()
-                }),
-            )
-            Button(onClick = { searchOnClick() }) {
-                Text(text = stringResource(R.string.main_screen_search_button_text))
+            Column(
+                modifier = Modifier
+                    .defaultMinSize(minHeight = 50.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TextField(
+                    modifier = Modifier
+                        .padding(horizontal = 24.dp, vertical = 8.dp)
+                        .fillMaxWidth(),
+                    value = text.value,
+                    onValueChange = { text.value = it },
+                    label = { Text(text = stringResource(R.string.main_screen_searchbar_label)) },
+                    keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
+                    keyboardActions = KeyboardActions(onSearch = {
+                        keyboardSearchOnClick()
+                    }),
+                )
+                Button(
+                    modifier = Modifier,
+                    onClick = { searchOnClick() }) {
+                    Text(text = stringResource(R.string.main_screen_search_button_text))
+                }
             }
         }
     }
@@ -85,8 +118,6 @@ fun StartScreen(
 @Preview(showBackground = true)
 @Composable
 fun StartScreenPreview() {
-    val text = rememberSaveable { mutableStateOf("") }
-    val viewModel = viewModel<LibraryViewModel>(factory = LibraryViewModel.Factory)
     BookShelfTheme {
     }
 }
