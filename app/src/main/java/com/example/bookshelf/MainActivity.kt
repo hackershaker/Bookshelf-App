cfee53abd2b7cea8ad01fc1191db1c43fa3a1aca
DY <deltahackall@gmail.com>
2024-05-02 16:50:05 +0900

    available edge to edge display and WindowCompat

--- app/src/main/java/com/example/bookshelf/MainActivity.kt
+++ app/src/main/java/com/example/bookshelf/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.bookshelf
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.bookshelf.ui.screen.LibraryViewModel
 import com.example.compose.BookShelfTheme
@@ -14,6 +16,8 @@ import com.example.compose.BookShelfTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             BookShelfTheme {
                 val viewModel: LibraryViewModel =
